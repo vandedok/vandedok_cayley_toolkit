f29285490367d6bd45c0264af9b5f397d511ff5c
Anton <anton.naymov@gmail.com>
2025-08-15 15:30:59 +0300

    Removed plotting from eval mode

--- python/vandedok_cayley_toolkit/ml/eval.py
+++ python/vandedok_cayley_toolkit/ml/eval.py
@@ -1,8 +1,7 @@
 from cayleypy import CayleyGraph, Predictor
 from tqdm.auto import tqdm
 import numpy as np
-import matplotlib.pyplot as plt
-from .cfg import CayleyMLCfg, SingleEvalCfg
+from .cfg import SingleEvalCfg
 
 
 def measure_success(
@@ -64,31 +63,3 @@ def evaluate(graph: CayleyGraph, eval_cfg: SingleEvalCfg, predictors: dict[str, 
         # for predictor_name in predictors.keys():
         #     success_rates[beam_w][predictor_name].append(success_rates_predictors[predictor_name])
     return success_counts, {k: np.array(v) / eval_cfg.n_trials for k, v in success_counts.items()}
-
-
-from statsmodels.stats.proportion import proportion_confint
-
-
-def show_eval_plots(success_counts, rw_lengths, beam_width, max_steps, n_trials, conf_alpha=0.95, puzzle_name=""):
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-
-    for predictor_name, counts in success_counts.items():
-        counts_plot = np.array(counts)
-        ax.plot(rw_lengths, counts_plot / n_trials, label=predictor_name, marker="o")
-
-        if conf_alpha is not None:
-            lower, upper = proportion_confint(count=counts_plot, nobs=n_trials, alpha=0.05, method="wilson")
-            ax.fill_between(x=rw_lengths, y1=lower, y2=upper, label=predictor_name, alpha=0.2)
-
-    ax.set_xticks(np.arange(0, max(rw_lengths) + 1, 50))  # Set minor ticks for x-axis
-    ax.set_yticks(np.arange(0, 1.1, 0.1))  # Set minor ticks for y-axis
-    ax.set_xlabel("Random Walk Length")
-    ax.set_ylabel("Success Rate")
-    ax.set_title(f"{puzzle_name}, beam size {beam_width}, max_steps: {max_steps}")
-    ax.legend()
-    # ax.set_xscale('log')
-    ax.grid(which="both")
-    ax.set_xlim(left=1, right=max(rw_lengths) + 1)
-    ax.set_ylim(bottom=-0.05, top=1.05)
-
-    return fig, ax
