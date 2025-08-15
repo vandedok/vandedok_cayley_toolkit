8dc79bd6df65ac1b248354d233a12ba39502df68
Anton <anton.naymov@gmail.com>
2025-08-15 17:14:04 +0300

    bug fix

--- python/vandedok_cayley_toolkit/ml/__init__.py
+++ python/vandedok_cayley_toolkit/ml/__init__.py
@@ -1,4 +1,4 @@
 from .cfg import CayleyMLCfg
 from .train import train_no_bellman, train_with_bellman
-from .eval import evaluate, show_eval_plots
+from .eval import evaluate
 from .utils import get_bfs_xy, get_rw_true_distances
